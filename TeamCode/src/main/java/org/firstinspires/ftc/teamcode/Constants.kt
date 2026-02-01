218a28cadf6bd80c0d8ad79bf50da131ef5491ca
Cooper Essary <cooper.essary@gmail.com>
2026-01-31 17:48:09 -0900

    pid things

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.kt
@@ -5,10 +5,10 @@ import com.acmerobotics.dashboard.config.Config
 
 @Config
 object RobotConstants {
-    @JvmField var DRIVE_P: Double = 1.1
-    @JvmField var DRIVE_I: Double = 0.3
+    @JvmField var DRIVE_P: Double = 0.8
+    @JvmField var DRIVE_I: Double = 0.0
     @JvmField var DRIVE_D: Double = 0.0
-    @JvmField var TURN_P: Double = 0.6
+    @JvmField var TURN_P: Double = 0.03
     @JvmField var TURN_I: Double = 0.0
     @JvmField var TURN_D: Double = 0.0
 }
